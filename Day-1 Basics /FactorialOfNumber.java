fb7e3b7b4dffee2d5efc583e28621a13b02204fc
Ayush Yeole <ayushyeole18@gmail.com>
2026-06-06 07:44:19 +0530

    Implement factorial calculation in Java

--- Day-1 Basics /FactorialOfNumber.java
+++ Day-1 Basics /FactorialOfNumber.java
@@ -1,1 +1,12 @@
-
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       System.out.print("Factorial: "+fact(5)); 
+    }
+    public static int fact(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+}
